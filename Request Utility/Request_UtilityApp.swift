b7ba4c6c9e2e6c049a5572060be9c535923fd8e7
Theodore Gaillard <teddy.gaillard@icloud.com>
2020-12-04 09:10:17 -0800

    Initial Commit

---
+++ Request Utility/Request_UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Request_UtilityApp.swift
+//  Request Utility
+//
+//  Created by Teddy Gaillard on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Request_UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
